fb8f9eca0c58a6923bec9f5095c7c02c9aeda4a3
Muhamad Ilham <miilhamm11@gmail.com>
2026-06-09 20:16:09 +0800

    menambahkan fitur edit untuk fakultas dan membuat pengecekan sederhana untuk memvalidasi biar nama fakultas yang dibuat tidak sama

--- application/controllers/Fakultas.php
+++ application/controllers/Fakultas.php
@@ -61,4 +61,65 @@ class Fakultas extends CI_Controller {
         $this->load->view('fakultas/form', $data);
         $this->load->view('layout/footer');
     }
-} 
+
+    public function ubah($id)
+    {
+        $record = $this->FakultasModel->getById($id);
+        if (!$record) {
+            $this->session->set_flashdata('swal', [
+                'icon'  => 'warning',
+                'title' => 'Gagal Memuat',
+                'text'  => 'Target data fakultas tidak ditemukan.'
+            ]);
+            redirect('fakultas');
+        }
+
+        $this->form_validation->set_rules(
+            'fakultas_name', 
+            'Nama Fakultas', 
+            'required|min_length[3]|max_length[100]|callback_check_nama_fakultas_unik['.$id.']',
+            [
+                'required'   => 'Kolom {field} tidak boleh dikosongkan.',
+                'min_length' => 'Nama Fakultas minimal diisi dengan 3 karakter.',
+                'max_length' => 'Nama Fakultas maksimal diisi dengan 100 karakter.'
+            ]
+        );
+
+        if ($this->form_validation->run() === TRUE) {
+            $payload = [
+                'fakultas_name' => $this->input->post('fakultas_name', true)
+            ];
+
+            $this->FakultasModel->update($id, $payload);
+            $this->session->set_flashdata('swal', [
+                'icon'  => 'success',
+                'title' => 'Update Sukses',
+                'text'  => 'Perubahan data fakultas telah diperbarui.'
+            ]);
+            redirect('fakultas');
+        }
+
+        $data['title'] = "Form Perubahan Fakultas";
+        $data['form_action'] = base_url('fakultas/ubah/' . $id);
+        $data['submit_label'] = 'Update';
+        $data['form_value'] = $record;
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('fakultas/form', $data);
+        $this->load->view('layout/footer');
+    }
+
+    public function check_nama_fakultas_unik($str, $id)
+    {
+        $this->db->where('fakultas_name', $str);
+        $this->db->where('fakultas_id !=', $id);
+        $query = $this->db->get('fakultas');
+
+        if ($query->num_rows() > 0) {
+            $this->form_validation->set_message('check_nama_fakultas_unik', '{field} sudah digunakan oleh fakultas lain, silakan gunakan nama berbeda.');
+            return FALSE;
+        }
+        
+        return TRUE;
+    }
+}
